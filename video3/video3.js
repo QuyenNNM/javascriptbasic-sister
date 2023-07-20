0b00b2a494b7f7363a2bdcd0e10aef728ae5ac11
sister112 <myquyen.230319@gmail.com>
2023-07-20 09:52:31 +0700

    Các kiểu dữ liệu

---
+++ video3/video3.js
@@ -0,0 +1,17 @@
+
+// int a = 5
+// float b = 6.9 => java
+//typeof (string))
+
+let x = 5;
+let y = 5;
+let z = 5;
+// const a = 6;
+
+
+
+console.log(`Hello '' "' world from javascript:`);
+// cách này hay
+console.log("Test string", 'x= ', x, 'y= ', y, 'z= ', z);
+// cách không hay
+console.log("Test string", 'x= ' + x + ' y= ' + y + ' z= ' + z);
