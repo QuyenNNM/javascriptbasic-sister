ba436ca6b893d53b1a9edca28ebbe795a5d0ee2a
sister112 <myquyen.230319@gmail.com>
2023-07-20 11:09:57 +0700

    #10 A Bờ Cờ NUmbers Trong Thế Giới Xi Đa

---
+++ video6/video6.js
@@ -0,0 +1,18 @@
+console.log('Hello world HTML'.toUpperCase());
+let a = 5;
+let b = 6;
+let c = 6.9;
+let d = '5';
+let e = Number('5')
+// cách hay dùng dấu cộng để hiểu là kiểu umber
+let h = +d;
+console.log(a + b)
+console.log(a + c)
+console.log(a * d)
+console.log(a + d)
+console.log(a - d)
+console.log(a / d)
+console.log(a === d)
+console.log(a === d, 'a: ', typeof a, 'd: ', typeof d)
+console.log('e: ', typeof e, 'e: ', typeof e)
+console.log('h: ', typeof h, 'h: ', typeof h)
