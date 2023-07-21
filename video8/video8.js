697bf7ebe61c3692d38b54cd295c06dd296f1929
sister112 <myquyen.230319@gmail.com>
2023-07-21 09:19:35 +0700

    #13 Kiểu Dữ Liệu Array - Tiêu Chuẩn Để Giao Tiếp Data

---
+++ video8/video8.js
@@ -0,0 +1,16 @@
+console.log('Hello world from HTML'.toLowerCase());
+let obj = {
+    name: 'Eric',
+    address: 'Ha Noi',
+    email: '',
+    gender: ''
+    // a: function () {
+    //     console.log('Hello world!');
+    //     return ''
+    // }
+};
+let b = 'Eric';
+console.log('What is your name ? ', `My name's : `, obj.name);
+console.log('Where are you from ? ', `I'm from : `, obj.address);
+// console.log('Call funtion : ', obj.a());
+console.log('type of a:', typeof obj, 'Type of b:', typeof b);
---
+++ video9/video9.js
@@ -0,0 +1,22 @@
+console.log('Hello world from HTML'.toLowerCase());
+let obj = {
+    name: 'Eric',
+    address: 'Ha Noi',
+    email: '',
+    gender: ''
+    // a: function () {
+    //     console.log('Hello world!');
+    //     return ''
+    // }
+};
+console.log('check: ', obj)
+
+let b = ['Mu', 'Chelsea', 'Liver'];
+console.log('type b:', typeof b, b);
+console.log('first item:', b[0], ' second item:', b[2])
+
+let family = [
+    { name: 'Eric', age: 25 },
+    { name: 'Monica', age: 24 }
+]
+console.log('my family: ', family)
