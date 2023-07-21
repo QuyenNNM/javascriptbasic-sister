c4d86ca8b82edf922f04d0c36a88c6081c08609a
sister112 <myquyen.230319@gmail.com>
2023-07-21 10:49:09 +0700

    #15 For Loops - Làm Quen Với Vòng Lặp For

---
+++ video10/video10.js
@@ -0,0 +1,9 @@
+console.log('Hello world from HTML'.toLowerCase());
+let a = 5;
+let b = 5;
+
+console.log('compare a, b:', 'a = ', a, 'b = ', b, 'result: ', a === b);
+console.log('compare a, b:', 'a = ', a, 'b = ', b, 'result: ', a == b);
+// === type, value
+// == value
+// undefined 
---
+++ video11/video11.js
@@ -0,0 +1,12 @@
+console.log('Hello world from HTML'.toLowerCase());
+
+// for (let i = 0; i < 10; i++) {
+//     console.log('check variable', i);
+// }
+let arr = ['Mu', 'ManCity', 'Liver', 'Chelsea']
+for (let i = 0; i < arr.length; i++) {
+    console.log('check variable i = ', arr[i]);
+}
+console.log('check variable i = ', i);
+
+// chỉ nên sử dụng biên let trong vòng lăp, vả thì phạm vi sử dụng của nó rộng hơn nên sẽ khó cho việc kiểm soát code
